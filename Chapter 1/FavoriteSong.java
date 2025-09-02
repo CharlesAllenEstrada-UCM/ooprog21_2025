efd35831d70d89ee60cd79a06aaec7c8a8fde398
CharlesAllenEstrada <charlesallenestrada1@gmail.com>
2025-09-02 18:26:29 +0800

    Create FavoriteSong.java

---
+++ Chapter 1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Yeah, what, what");
+        System.out.println("You cannot hop in my car");
+        System.out.println("Bentley coupe riding with stars");
+        System.out.println("Yeah, what, what!");
+        System.out.println("I keep a Glock like a cop");
+        System.out.println("Ayy, yeah (yeah)");
+        System.out.println("She give the top in the drop");
+    }
+}
